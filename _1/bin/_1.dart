34053fe5759433a9679c129495fedebba48232b0
sanazar777 <sanazars.ss@gmail.com>
2025-06-14 11:44:01 +0300

    Add files via upload

---
+++ _1/bin/_1.dart
@@ -0,0 +1,5 @@
+import 'package:_1/_1.dart' as _1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${_1.calculate()}!');
+}
---
+++ _1/test/_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:_1/_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
